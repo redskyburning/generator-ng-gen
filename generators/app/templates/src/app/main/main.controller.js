6279049b1d03b7d8ce3fbf51a301c2fac5f35cd2
Ben Webb <ben@redskyburning.com>
2017-06-14 13:29:18 -0400

    wip

--- generators/app/templates/src/app/main/main.controller.js
+++ generators/app/templates/src/app/main/main.controller.js
@@ -1,9 +1,9 @@
 export class MainController {
-  constructor ($log) {
-    'ngInject';
+	constructor($log) {
+		'ngInject';
 
-    this.$log = $log;
+		this.$log = $log;
 
-    this.foo = 'bar';
-  }
+		this.foo = 'bar';
+	}
 }
