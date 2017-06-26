a459887041656bb0100d47a8accbd47f90b47e0c
Ben Webb <ben@redskyburning.com>
2017-06-25 20:13:40 -0400

    error handler

--- generators/app/templates/src/app/controllers/error/error.controller.spec.js
+++
@@ -1,9 +0,0 @@
-describe('controller ErrorController', () => {
-	let vm;
-
-	beforeEach(angular.mock.module('test'));
-
-	beforeEach(inject(($controller) => {
-		vm = $controller('ErrorController');
-	}));
-});
